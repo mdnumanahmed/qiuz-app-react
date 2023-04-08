464596aa3b24d80e9cb2c9971ffc8b8c935a86e9
mdnumanahmed <mnumansyl@gmail.com>
2023-04-09 04:08:53 +0600

    setup tailwindcss and daisyui

---
+++ src/assets/resources.js
@@ -0,0 +1,5 @@
+// Get all quiz topics: https://openapi.programming-hero.com/api/quiz
+
+// Get topic details by topic id: https://openapi.programming-hero.com/api/quiz/${id}
+
+// Example: https://openapi.programming-hero.com/api/quiz/1
